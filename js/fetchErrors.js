b75620d063c6b8ad7cac8ceab653306881198138
Andrii Danylko <46224717+riko1212@users.noreply.github.com>
2023-06-29 22:02:46 +0300

    Update fetchErrors.js

--- js/fetchErrors.js
+++ js/fetchErrors.js
@@ -1,5 +1,5 @@
 const fetchErrors = () => {
-  return fetch('http://www.mocky.io/v2/5dfcef48310000ee0ed2c281').then(
+  return fetch('https://www.mocky.io/v2/5dfcef48310000ee0ed2c281').then(
     (response) => {
       return response.json();
     }
